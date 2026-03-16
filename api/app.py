9811f6fa5309c022793954186aaf20bbcc5fed97
sajanmajrekar <sajanmajrekar14@gmail.com>
2026-03-16 15:53:00 +0530

    Add Vercel handler

--- api/app.py
+++ api/app.py
@@ -1,10 +1,5 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(__file__) + "/..")
-
-from app import app
-
-# Vercel serverless entrypoint
-def handler(request, response):
-    return app(request.environ, response.start_response)
+def handler(request):
+    return {
+        "statusCode": 200,
+        "body": "WatchBanner API running on Vercel"
+    }
