1bdf7751d6b61376c164d1a5b7f49a05d24102aa
sajanmajrekar <sajanmajrekar14@gmail.com>
2026-03-16 15:29:12 +0530

    sajan change

--- api/app.py
+++ api/app.py
@@ -5,4 +5,6 @@ sys.path.append(os.path.dirname(__file__) + "/..")
 
 from app import app
 
-handler = app
+# Vercel serverless entrypoint
+def handler(request, response):
+    return app(request.environ, response.start_response)
